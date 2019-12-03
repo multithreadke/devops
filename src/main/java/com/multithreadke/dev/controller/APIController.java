1205066b8f71a4d609762711cb2755f06f1ce6be
Enrique Cherry <multithreadke@Enriques-MacBook-Pro.local>
2019-12-03 22:33:43 +0300

    import optimization executed

--- src/main/java/com/multithreadke/dev/controller/APIController.java
+++ src/main/java/com/multithreadke/dev/controller/APIController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping(value = "/api/v1")
 public class APIController {
@@ -17,11 +14,7 @@ public class APIController {
     @GetMapping(value = "/status")
     public ResponseEntity<?> queryStatus()
     {
-        Map<String, String> map = new HashMap<>();
-        map.put("error", "False");
-        map.put("statusCode", "200");
-        map.put("statusMessage", "Success");
-        return new ResponseEntity<>(new Result(true, "200", "Success"), HttpStatus.OK);
+        return new ResponseEntity<>(new Result(false, "200", "Success"), HttpStatus.OK);
     }
 
 }
