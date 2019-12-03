4e91fa902b1311a18381f3f33d512ae5008d7b0e
Enrique Cherry <multithreadke@Enriques-MacBook-Pro.local>
2019-12-03 14:37:04 +0300

    Initial Commit

---
+++ src/main/java/com/multithreadke/dev/DevopClientApplication.java
@@ -0,0 +1,14 @@
+package com.multithreadke.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DevopClientApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multithreadke/dev/DevopClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multithreadke.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
